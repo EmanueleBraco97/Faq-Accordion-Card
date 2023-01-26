22066a57263a24f30bd28e79b0c5cf5eddd60a80
Emanuele Braco <emanuele.braco@gmail.com>
2023-01-26 18:08:15 +0100

    initialize new project with tailwind and vite

---
+++ faq-accordion-card/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        mobile: "320px",
+      },
+    },
+  },
+  plugins: [],
+};
